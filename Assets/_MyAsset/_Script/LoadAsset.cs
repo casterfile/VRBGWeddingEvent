070910322e874a500ae28d9b6a14d9637bd97d35
casterfile <dev.anthonycastor@gmail.com>
2017-11-21 16:16:12 +0800

    Update

--- Assets/_MyAsset/_Script/LoadAsset.cs
+++ Assets/_MyAsset/_Script/LoadAsset.cs
@@ -7,21 +7,21 @@ using UnityEngine.UI;
 public class LoadAsset : MonoBehaviour {
     // Use this for initialization
 	void Start () { 
-//        string url = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/09/spheresample";
+//      string url = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/09/spheresample";
 //		string url = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/11/consoleoutput";
 //		string url = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/11/vrassetstream";
 
 		#if UNITY_IOS
-		string url = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/11/vrassetstream";
+			string url = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/11/vrassetstream";
 		#endif
 
 		#if UNITY_ANDROID
 //			string url = "https://wptest.bgbridalgallery.com.ph/files/jp/Streaming/Android/vrassetstream";
-		string url = "https://wptest.bgbridalgallery.com.ph/files/BundlePOP/asset";
+			string url = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/11/vrassetstream";
 		#endif
 
         WWW www = new WWW(url);
-		StartCoroutine (WaitforReq(www, "asset"));
+		StartCoroutine (WaitforReq(www, "VRAssetStream"));
         
         print("Start Asset Loaded");
     }
