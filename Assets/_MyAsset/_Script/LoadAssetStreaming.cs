4e08f997fd342e3022c86af8c7ed0cf225e08cad
casterfile <dev.anthonycastor@gmail.com>
2017-11-21 10:09:45 +0800

    Update

---
+++ Assets/_MyAsset/_Script/LoadAssetStreaming.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class LoadAssetStreaming : MonoBehaviour {
+
+	//Code for unity 2017.2
+	/*
+	void Start() {
+		StartCoroutine(GetAssetBundle());
+	}
+
+	IEnumerator GetAssetBundle() {
+		UnityWebRequest www = UnityWebRequest.GetAssetBundle("http://www.my-server.com/myData.unity3d");
+		yield return www.SendWebRequest();
+
+		if(www.isNetworkError || www.isHttpError) {
+			Debug.Log(www.error);
+		}
+		else {
+			AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(www);
+		}
+	}
+	*/
+}
