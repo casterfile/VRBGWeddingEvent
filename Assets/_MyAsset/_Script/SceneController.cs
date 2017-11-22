40a583ecdbbac8f132077890c78c45c4fe4645f1
casterfile <dev.anthonycastor@gmail.com>
2017-11-22 15:42:08 +0800

    Update

--- Assets/_MyAsset/_Script/SceneController.cs
+++ Assets/_MyAsset/_Script/SceneController.cs
@@ -15,21 +15,32 @@
 namespace GoogleVR.GVRDemo {
   	using UnityEngine;
 	using System.Collections;
+	using UnityEngine.Video;
+	using UnityEngine.UI;
 
 	public class SceneController : MonoBehaviour {
 		private  GameObject[] objVideo,objVideoButton;
-		private GameObject BackgroundPicture, BackButton;
+		private  VideoPlayer[] VideoPlayerControl;
+		private string[] VideoURL;
+		private GameObject BackgroundPicture, BackButton,VideoPanelContent;
 		private float timeMouseDown = 0.0f;
 		private int totalLoop = 0;
 		private string NameAction = "";
+		private int IntContentMove = 0;
 		private int IntAction = -1;
+
+		public RectTransform ContentVideos;
+
 		public bool mouseDown = false;
 	    void Start() {
 			int ArrayLength = 3;
 			totalLoop = ArrayLength - 1;
 			objVideo = new GameObject[ArrayLength];
 			objVideoButton = new GameObject[ArrayLength];
+			VideoURL = new string[ArrayLength];
+			VideoPlayerControl = new VideoPlayer[ArrayLength];
 			FindAllVideos ();
+			URLSetting ();
 
 			BackButton.SetActive (false);
 	    }
@@ -37,14 +48,20 @@ namespace GoogleVR.GVRDemo {
 		void Update() {
 			if (mouseDown == true) {
 				timeMouseDown += Time.deltaTime;
-				print ("timeMouseDown: "+timeMouseDown);
+//				print ("timeMouseDown: "+timeMouseDown);
 				if(timeMouseDown >= 3){
 					if(NameAction == "Video"){
-						StartCoroutine(Controller("Video",IntAction));
+						Controller("Video",IntAction);
 						OnPointerUp ();
 					}else if(NameAction == "BackMain"){
-						StartCoroutine(Controller("BackMain",1));
+						Controller("BackMain",1);
 						OnPointerUp ();
+					}else if(NameAction == "ContentMoveUp"){
+						Controller ("ContentMoveUp", 1);
+						timeMouseDown = 0;
+					}else if(NameAction == "ContentMoveDown"){
+						Controller ("ContentMoveDown", 1);
+						timeMouseDown = 0;
 					}
 				}
 			}
@@ -54,19 +71,22 @@ namespace GoogleVR.GVRDemo {
 			mouseDown = true;
 			NameAction = "Video";
 			IntAction = number;
-//			if(timeMouseDown >= 3){
-//				StartCoroutine(Controller("Video",number));
-//			}
 
 		}
 
 		public void SceneBackMain() {
 			mouseDown = true;
 			NameAction = "BackMain";
-//			if(timeMouseDown >= 3){
-//				StartCoroutine(Controller("BackMain",1));
-//			}
+		}
 
+		public void ContentMoveUp(){
+			mouseDown = true;
+			NameAction = "ContentMoveUp";
+		}
+
+		public void ContentMoveDown(){
+			mouseDown = true;
+			NameAction = "ContentMoveDown";
 		}
 
 		public void OnPointerUp(){
@@ -74,21 +94,51 @@ namespace GoogleVR.GVRDemo {
 			timeMouseDown = 0;
 		}
 
-		IEnumerator Controller(string Output,int OutputNumber)
+		private void Controller(string Output,int OutputNumber)
 		{
-			HideAllVideos ();
+			
 			if(Output == "Video"){
+				HideAllVideos ();
 				objVideo[OutputNumber].SetActive (true);
 				BackButton.SetActive (true);
 				BackgroundPicture.SetActive (false);
+				VideoPanelContent.SetActive (false);
 			}
 
 			if(Output == "BackMain"){
+				HideAllVideos ();
 				ShowAllVideosButtons ();
 				BackButton.SetActive (false);
+				VideoPanelContent.SetActive (true);
 				BackgroundPicture.SetActive (true);
 			}
-			yield return new WaitForSeconds(1.5f);
+
+			if(Output == "ContentMoveUp"){
+				if(IntContentMove == 0 ){
+					IntContentMove = 1;
+					ContentVideos.anchoredPosition = new Vector2(0.0f, 220.0f);
+				}
+				else if(IntContentMove == 1){
+					IntContentMove = 2;
+					ContentVideos.anchoredPosition = new Vector2(0.0f, 430.0f);
+					OnPointerUp ();
+				}
+
+			}
+
+			else if(Output == "ContentMoveDown"){
+				if(IntContentMove == 1 ){
+					IntContentMove = 0;
+					ContentVideos.anchoredPosition = new Vector2(0.0f, 0.0f);
+					OnPointerUp ();
+				}
+				else if(IntContentMove == 2){
+					IntContentMove = 1;
+					ContentVideos.anchoredPosition = new Vector2(0.0f, 220.0f);
+				}
+			}
+
+			print ("IntContentMove: "+ IntContentMove);
 
 		}
 
@@ -109,10 +159,30 @@ namespace GoogleVR.GVRDemo {
 		private void FindAllVideos(){
 			for (int x = 0; x <= totalLoop; x++) {
 				objVideo[x] =  GameObject.Find("VRAssetStream/GamePlayAsset/VRGame/VideoList/Videos ("+x+")");
+				VideoPlayerControl[x] =  objVideo[x].GetComponent<VideoPlayer>();
+				objVideoButton[x] =  GameObject.Find("VideoPanel ("+x+")");
+
 				BackgroundPicture =  GameObject.Find("VRAssetStream/GamePlayAsset/VRGame/Photo");
 				BackButton =  GameObject.Find("VRAssetStream/GamePlayAsset/VRGame/SceneController/BackButton");
-				objVideoButton[x] =  GameObject.Find("VRAssetStream/GamePlayAsset/VRGame/SceneController/VideoPanel ("+x+")");
+				VideoPanelContent = GameObject.Find ("VideoPanelContent");
 			}
+		}
+
+
+
+		void URLSetting(){
+			VideoURL[0] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+			VideoURL[1] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+			VideoURL[2] = "https://wptest.bgbridalgallery.com.ph/wp-content/uploads/2017/10/Sample-Video.mp4";
+
+			VideoPlayerControl [0].source = VideoSource.Url;
+			VideoPlayerControl [0].url = VideoURL[0];
+
+			VideoPlayerControl [1].source = VideoSource.Url;
+			VideoPlayerControl [1].url = VideoURL[1];
+
+			VideoPlayerControl [2].source = VideoSource.Url;
+			VideoPlayerControl [2].url = VideoURL[2];
 		}
 
   }
